49f42aa106b3f478f9f4a5a9d561ecaf90051de4
tzmf <qiqizhangch@gmail.com>
2019-11-29 21:48:21 +0800

    Init: Architecture

--- promise.js
+++ promise.js
@@ -1,24 +1,93 @@
-const PENDING  = Symbol("pending")
-const FULFILLED = Symbol("fulfilled")
-const REJECTED = Symbol("rejected")
+/**
+ * Promises/A+ Implement
+ *
+ * @export
+ * @class MyPromise
+ */
 
-class MyPromise {
-	constructor(fn) {
-		this.state = PENDING
-		fn.call(this.onFulfilled, this.onRejected)
+export default class MyPromise {
+  constructor(fn) {
+		if (typeof fn !== 'function') {
+			const type = typeof fn
+			throw new Error(`Uncaught TypeError: Promise resolver ${type} is not a function`)
+		}
+
+		this.state = 'pending'
+		this.resolvedCallbacks = []
+		this.rejectedCallbacks = []
+		try {
+			fn.call(undefined, this._onFulfilled, this._onRejected)
+		} catch(e) {
+			// todo
+		}
 	}
 
-	onFulfilled(value) {
-	}
+  /**
+   * Register Callback function
+   * return a MyPromise
+   *
+   * @param {function} onFulfilled
+   * @param {function} onRejected
+   *
+   * @memberof MyPromise
+   */
 
-	onRejected(reason) {
-	}
+  then(onFulfilled, onRejected) {
+    try {
+      return MyPromise.resolve();
+    } catch (e) {
+      return MyPromise.reject();
+    }
+  }
 
-	transition() {
-	}
-	
-	then() {}
+  /**
+   *
+   *
+   * @param {*} onRejected
+   * @memberof MyPromise
+   */
 
-	catch() {}
+  catch(onRejected) {}
+
+  /**
+   * deal with fulfilled callback
+   *
+   * @memberof MyPromise
+   */
+
+  _onFulfilled(value) {}
+
+  /**
+   * deal with rejected callback
+   *
+   * @memberof MyPromise
+   */
+
+  _onRejected(reason) {}
 }
 
+/**
+ * 返回一个状态为 `fulfilled` 的 `MyPromise` 实例
+ *
+ * @method resolve
+ * @return {MyPromise}
+ */
+
+MyPromise.resolve = function(value) {
+  return new MyPromise(resolved => {
+    resolved(value);
+  })
+}
+
+/**
+ * 返回一个状态为 `rejected` 的 `MyPromise` 实例
+ *
+ * @method reject
+ * @return {MyPromise}
+ */
+
+MyPromise.reject = function(reason) {
+  return new MyPromise((null, rejected) => {
+    rejected(reason)
+  })
+}
